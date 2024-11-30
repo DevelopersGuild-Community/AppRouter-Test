7ed34f2941d38c419070c809b15b6d10e296eebe
masatotezuka <tez.0731.mst@gmail.com>
2024-11-30 16:04:11 +0900

    add form normal test

--- app/ui/invoices/create-form.test.tsx
+++ app/ui/invoices/create-form.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react"
 import CreateForm from "./create-form"
 import { screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 describe("CreateForm", () => {
   beforeEach(() => {
@@ -28,5 +29,39 @@ describe("CreateForm", () => {
   test("formが表示される", () => {
     render(<CreateForm customers={[]} />)
     expect(screen.getByLabelText("Choose customer")).toBeInTheDocument()
+  })
+
+  test("フォームに正常な値が入力される", async () => {
+    render(
+      <CreateForm
+        customers={[
+          {
+            id: "1",
+            name: "John Doe",
+          },
+          {
+            id: "2",
+            name: "Hannah Montana",
+          },
+        ]}
+      />
+    )
+    const selectBox = screen.getByRole("combobox")
+    await userEvent.selectOptions(selectBox, "John Doe")
+    expect(selectBox).toHaveValue("1")
+
+    const input = screen.getByRole("spinbutton")
+    await userEvent.type(input, "100")
+    expect(input).toHaveValue(100)
+
+    const radios = screen.getAllByRole("radio")
+    await userEvent.click(radios[0])
+    expect(radios[0]).toBeChecked()
+    const submitButton = screen.getByRole("button", {
+      name: "Create Invoice",
+    })
+    await userEvent.click(submitButton)
+
+    // expect(screen.getByText("Something went wrong")).toBeInTheDocument()
   })
 })
